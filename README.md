2255e6af94eeb2ab9bfe3d9313b1b0237dae512b
nilou <githubppv@gmail.com>
2026-02-28 10:38:20 +0100

    last commit for now, it kind of bored me

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # A Toy Terminal Emulator
 ###### Toy Terminal -> ToyTerminal -> ToyTerm -> Tterm
+
+[ ] TODO: try GLFW with #define GLFW_NO_API
+[ ] TODO: check for GPU and if there is use glx to render
--- Tterm.c
+++ Tterm.c
@@ -1,7 +1,3 @@
-//TODO:try GLFW with #define GLFW_NO_API
-//TODO: check for GPU and if there is use glx to render
-
-//TODO: WRITE THINGS TO TERMINAL
 #define _XOPEN_SOURCE 600
 #define _GNU_SOURCE
 
@@ -9,8 +5,11 @@
 #include <fcntl.h>//manipulate fds
 #include <stdlib.h>
 #include <string.h>
+
 #include <pty.h>
 #include <X11/Xlib.h>
+#include <X11/keysym.h>
+
 #include <assert.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -27,15 +26,10 @@ bool xorg_exists()
 
 	return true;
 }
-/*
- * A me serve creare un nuovo processo(?) con execv o qualcosa di simile
- * quindi mi serve il PID dello slave(?)
- * poi devo fare in modo renderizzare i caratteri su term_window(), usando gli eventi
- * successivamente devo mandare quei comandi allo shell(bash) e stampare l'output
- * quindi devi connettermi a stdin e stdout e stderr per eventuali cazzi
-*/
+
 int main(void)
-{   
+{  
+// SETTING UP XORG
 	if(!xorg_exists())//TODO: check to see if it works
 	{
 		assert("FATAL ERROR: X server is NOT running\nor the 'DISPLAY' enviromental variable is not set properly");
@@ -69,9 +63,9 @@ int main(void)
                                                BG);
 
 	
-	XSelectInput(display, simple_window, KeyPressMask | ExposureMask);//specifies the input type that X should report, if no make -> no report on input
+	XSelectInput(display, simple_window, KeyPressMask | KeyReleaseMask | ExposureMask);//specifies the input type that X should report, if no make -> no report on input
 
- 	int window_name = XStoreName(display, simple_window, "Tterm");
+    UNUSED int window_name = XStoreName(display, simple_window, "Tterm");
 
 	XMapWindow(display, simple_window);//map window to be drawn
 
@@ -80,39 +74,7 @@ int main(void)
     
 	XSync(display, false);
 
-#if 0
-//open an unused master file descriptor for master pseduto-terminal 
-
-    int master_fd = posix_openpt(O_RDWR);//from /dev/pts/ptmx pseudo-term multiplexter 
-    
-    if(master_fd == -1)
-    {
-        perror("ERROR: Could not open master device\n");
-        return 1; 
-    }
-
-    //it changes the ID of the slave to the real UID of the calling process, so Tterm in this case 
-    grantpt(master_fd);
-    unlockpt(master_fd); 
-
-    // Once both the pseudoterminal master and slave are open
-    // the slave provides a processe with an interface that is identical to that of a real terminal.   
-    char* slave_name = ptsname(master_fd); 
-    int slave_fd = open(slave_name, O_RDWR|O_NOCTTY);//offline man is wrong, return value > 0 is good
-    
-    if(slave_fd < 0)
-    {
-        perror("ERROR: Could not open slave device\n");
-        return -1;
-    }
-    else
-    {
-        char* slave_to_shell = getenv("SHELL");//execute shell in a separete process
-        char* arg = "";
-    
-        execlp(slave_to_shell, arg, NULL);
-    }
-#endif
+// CONNECTION TO SHELL
 
     int master_fd;  // primary file descriptor for the pseudo-terminal
     pid_t pid;      // Process ID of the child process
@@ -141,8 +103,11 @@ int main(void)
         exit(1);
     }
 
+// RENDERING
+    bool loop = true;
+    
     Atom wm_delete_window = XInternAtom(display, "WM_DELETE_WINDOW", False);
-    Status set_atom_protocols = XSetWMProtocols(display, simple_window, &wm_delete_window, 1);
+    UNUSED Status set_atom_protocols = XSetWMProtocols(display, simple_window, &wm_delete_window, 1);
 
     XEvent event;
     int exit_flag = 1;
@@ -152,38 +117,43 @@ int main(void)
     
     XSetBackground(display, graphical_ctx, black);
     XSetForeground(display, graphical_ctx, white);
-   
-    while(exit_flag != 0)
-    {
-        XNextEvent(display, &event);
-        switch(event.type)
-        {
-            case KeyPress:
-                {
-                    printf("Key Pressed!\n");
-                }
-            break;
 
-            case ClientMessage:
+    while(loop)
+    {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(master_fd, &fds);
+        
+        while(exit_flag != 0)
+        {
+            XNextEvent(display, &event);
+            switch(event.type)
+            {
+                case KeyPress:
                 {
-                    if((Atom)event.xclient.data.l[0] == wm_delete_window)
+                    KeyCode key = event.xkey.keycode;
+                    char* character = XKeysymToString( XKeycodeToKeysym(display, key, 0) );
+                case Expose:
+                {
+                    XDrawString(display, simple_window, graphical_ctx, 50, 50, character, strlen(character));
+                }
+                break;//come posso separare i caratteri? Devo sicuramente creare una grid come quella di RefTerm, non mi va
+                
+                case ClientMessage:
+                {
+                    if( (Atom)event.xclient.data.l[0] == wm_delete_window )
                     {
                         exit_flag = 0;
+                        loop = false;
                     }
                 }
-            break;
-            
-            case Expose:
-                {
-                    char* msg = "STOP THIS NIGHTMARE";
-                    XDrawString(display, simple_window, graphical_ctx, 50, 50, msg, strlen(msg));
+                break;
                 }
-            break;
+            }
         }
     }
-
+    
     XCloseDisplay(display);
     return 0;
 
 }
-
